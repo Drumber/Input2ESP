56178a0b77b4179f5fa49aac303c9fe3b1d92db2
Lars <29163322+Drumber@users.noreply.github.com>
2021-05-03 17:18:04 +0200

    added about section and icons to settings fragment

--- app/src/main/java/com/github/drumber/input2esp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/github/drumber/input2esp/ui/settings/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package com.github.drumber.input2esp.ui.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.InputType
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.EditTextPreference
 import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.github.drumber.input2esp.R
 
@@ -24,6 +27,16 @@ class SettingsFragment: PreferenceFragmentCompat() {
         // accept only number values for 'default command delay' preference
         findPreference<EditTextPreference>(getString(R.string.prefkey_default_command_delay))?.setOnBindEditTextListener {
             it.inputType = InputType.TYPE_CLASS_NUMBER
+        }
+
+        val appVersion = requireContext().packageManager.getPackageInfo(requireContext().packageName, 0)
+        findPreference<Preference>(getString(R.string.prefkey_app_version_info))?.summary = appVersion.versionName
+
+        findPreference<Preference>(getString(R.string.prefkey_app_repo_info))?.setOnPreferenceClickListener {
+            // open repository url in browser
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.app_repo_url)))
+            startActivity(intent)
+            true
         }
     }
 }
